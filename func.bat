a5db576a9fbfe174674dc12c445a958d24d0d322
Alexander Demachev <berserktv@yandex.ru>
2023-03-28 22:11:36 +0300

    add install Nvidia.GeForceExperience (from WinGet)

--- func.bat
+++ func.bat
@@ -4,7 +4,10 @@ REM echo "Install Python 3.10 Environment"
 REM winget install -e --id=Python.Python.3.10
 REM winget install -e --id=Microsoft.VCRedist.2015+.x86
 REM cmd
-REM set PATH=%PATH%;C:\Users\Admin\AppData\Local\Programs\Python\Python310\;
+REM set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python310\;
+REM set PYTHON=%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python310\python
+REM winget install -e --id=Nvidia.GeForceExperience
+
 
 cd %HOMEDRIVE%%HOMEPATH%
 REM git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
