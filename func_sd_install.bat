c0ee4c23438fb95d65064aed5a2cb0048dd43cb6
Alexander Demachev <berserktv@yandex.ru>
2023-03-29 19:50:19 +0300

    add section  sd-install for Windows 10

--- func_sd_install.bat
+++ func_sd_install.bat
@@ -1,16 +1,13 @@
 @echo on
 
-REM echo "Install Python 3.10 Environment"
-REM winget install -e --id=Python.Python.3.10
-REM winget install -e --id=Microsoft.VCRedist.2015+.x86
-REM cmd
-REM set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python310\;
-REM set PYTHON=%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python310\python
-REM winget install -e --id=Nvidia.GeForceExperience
+echo "Install Python 3.10 Environment"
+winget install -e --id=Python.Python.3.10
+winget install -e --id=Microsoft.VCRedist.2015+.x64
 
+set PATH=%PATH%;%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python310\;
+set PYTHON=%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python310\python.exe
 
 cd %HOMEDRIVE%%HOMEPATH%
-REM git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 cd stable-diffusion-webui
-.\webui.bat
-
+call webui-user.bat
