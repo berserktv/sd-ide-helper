de4d842349573e1ae0beaeecd562adcb9b52ea52
Alexander Demachev <berserktv@yandex.ru>
2023-03-30 18:53:24 +0300

    simplified, removed label GOTO

--- func_sd_without_gpu.bat
+++ func_sd_without_gpu.bat
@@ -11,20 +11,22 @@ cd %HOMEDRIVE%%HOMEPATH%
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 cd stable-diffusion-webui
 
-IF %1 NEQ no-GPU GOTO СallWebui
-
 set outfile=webui-user.bat
 
-echo @echo off>%outfile%
-echo. >>%outfile%
-echo set PYTHON=>>%outfile%
-echo set GIT=>>%outfile%
-echo set VENV_DIR=>>%outfile%
-echo set COMMANDLINE_ARGS=--skip-torch-cuda-test --no-half>>%outfile%
-echo. >>%outfile%
-echo call webui.bat>>%outfile%
+@echo off
+IF %1==no-GPU (
+    echo @echo off>%outfile%
+    echo. >>%outfile%
+    echo set PYTHON=>>%outfile%
+    echo set GIT=>>%outfile%
+    echo set VENV_DIR=>>%outfile%
+    echo set COMMANDLINE_ARGS=--skip-torch-cuda-test --no-half>>%outfile%
+    echo. >>%outfile%
+    echo call webui.bat>>%outfile%
 
-:СallWebui
+) ELSE (
+    REM turn off --skip-torch-cuda-test, if left in COMMANDLINE_ARGS (default)
+    git checkout %outfile%
+)
+
 call webui-user.bat
-
-:ExitBat
